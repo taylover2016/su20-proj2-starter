adaf3565512f71f86f25fc660b8c4470cabadcce
taylover2016 <taylover2016@yahoo.com>
2023-01-17 22:57:58 +0800

    ReLU implemented

--- src/relu.s
+++ src/relu.s
@@ -15,24 +15,28 @@
 relu:
     # Prologue
 
+    # Check the length of the array
+    addi t0, x0, 1 
+    bge a1, t0, loop_start # if a1 >= t0 then jump to loop_start
+    addi a1, x0, 8
+    j exit2
 
 loop_start:
+    # Use a1 to loop
+    beq a1, x0, loop_end
+    addi a1, a1, -1
     
+    # Use slli to index into memory
+    slli t0, a1, 2
+    add t0, t0, a0
 
-
-
-
-
-
-
-loop_continue:
-
-
+    # Read in the number and revise it if necessary
+    lw t1, 0(t0)
+    bge t1, x0, loop_start # if x0 <= t1 then no need to revise
+    sw x0, 0(t0)
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
 
-    
 	ret
