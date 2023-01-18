53ae668bcd859687707cd7346de2f5645c0df693
taylover2016 <taylover2016@yahoo.com>
2023-01-18 11:34:58 +0800

    Argmax implemented

--- src/argmax.s
+++ src/argmax.s
@@ -17,25 +17,44 @@
 argmax:
 
     # Prologue
+    # s0 -> index of the largest element
+    # s1 -> largest value
+    addi sp, sp, -8
+    sw s0, 8(sp)
+    sw s1, 4(sp)
 
+    # Check the length of the array
+    addi t0, x0, 1 
+    bge a1, t0, loop_start # if a1 >= t0 then jump to loop_start
+    addi a1, x0, 7
+    j exit2
+
+    # Set the initial value for the largest element and the index
+    add s0, x0, x0
+    lw s1, 0(a0)
 
 loop_start:
+    # Use a1 to loop
+    beq a1, x0, loop_end
+    addi a1, a1, -1
+    
+    # Use slli to index into memory
+    slli t0, a1, 2
+    add t0, t0, a0
 
-
-
-
-
-
-
-
-loop_continue:
-
-
+    # Read in the number and compare it with the current largest one
+    lw t1, 0(t0)
+    blt t1, s1, loop_start # if t1 <= largest_val then no need to update
+    
+    # Update the value and the index
+    add s1, t1, x0
+    add s0, a1, x0
+    j loop_start
 
 loop_end:
-    
-
+    add a0, s0, x0
     # Epilogue
-
-
+    lw s0, 8(sp)
+    lw s1, 4(sp)
+    addi sp, sp, 8
     ret
