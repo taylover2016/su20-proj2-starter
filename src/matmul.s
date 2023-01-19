676578e514f98a3111be513ddd3e13427c6bf247
taylover2016 <taylover2016@yahoo.com>
2023-01-19 17:38:52 +0800

    Matrix multiplication implemented

--- src/matmul.s
+++ src/matmul.s
@@ -25,38 +25,112 @@
 matmul:
 
     # Error checks
-
+    # Check the dimensions of m0
+    li t0, 1
+    beq a1, t0, exception_m0
+    beq a2, t0, exception_m0
+    beq a4, t0, exception_m1
+    beq a5, t0, exception_m1
+    bne a2, a4, exception_match
 
     # Prologue
+    # s0 -> current sum of the product
+    # s1 -> current element of the final matrix product
+    # ra -> need to call "dot"
+    # t0 -> row index of the first matrix
+    # t1 -> col index of the second matrix
+    addi sp, sp, -12
+    sw s1, 8(sp)
+    sw s0, 4(sp)
+    sw ra, 0(sp)
 
+    add s0, x0, x0
+    li t0, 0
+    li t1, 0
 
 outer_loop_start:
-
-
-
+    beq t0, a1, outer_loop_end
 
 inner_loop_start:
+    beq t1, a5, inner_loop_end
 
+    # Save a0-a4, t0-t1, and ra
+    addi sp, sp, -32
+    sw a0, 28(sp)
+    sw a1, 24(sp)
+    sw a2, 20(sp)
+    sw a3, 16(sp)
+    sw a4, 12(sp)
+    sw t0, 8(sp)
+    sw t1, 4(sp)
+    sw ra, 0(sp)
 
+    # Set the arguments
+    # Calculate the address first
+    mul t2, t0, a2
+    add t3, t1, x0
+    slli t2, t2, 2
+    slli t3, t3, 2
+    add t2, t2, a0
+    add t3, t3, a3
 
+    # Push the arguments and call the dot function
+    add a0, t2, x0
+    add a1, t3, x0
+    add a2, a2, x0
+    li a3, 1
+    add a4, a5, x0
 
+    jal dot  # jump to dot and save position to ra
+    
+    # Save the result and restore the registers
+    add s1, a0, x0
+    
+    lw a0, 28(sp)
+    lw a1, 24(sp)
+    lw a2, 20(sp)
+    lw a3, 16(sp)
+    lw a4, 12(sp)
+    lw t0, 8(sp)
+    lw t1, 4(sp)
+    lw ra, 0(sp)
+    addi sp, sp, 32
+    
+    # Calculate the corresponding position in memory
+    # At row t0, col t1
+    mul t2, t0, a5
+    add t3, t2, t1
+    slli t3, t3, 2
+    add t3, t3, a6
+    sw s1, 0(t3)
 
-
-
-
-
-
-
+    addi t1, t1, 1
+    j inner_loop_start
 
 inner_loop_end:
-
-
-
+    addi t0, t0, 1
+    add t1, x0, x0
+    j outer_loop_start
 
 outer_loop_end:
-
+    
 
     # Epilogue
-    
+    lw s1, 8(sp)
+    lw s0, 4(sp)
+    lw ra, 0(sp)
+    addi sp, sp, 12
     
     ret
+
+exception_m0:
+    addi a1, x0, 2
+    j exit2
+
+exception_m1:
+    addi a1, x0, 3
+    j exit2
+
+exception_match:
+    addi a1, x0, 4
+    j exit2
--- tests/assembly/test_matmul.s
+++ tests/assembly/test_matmul.s
@@ -11,19 +11,28 @@ d: .word 0 0 0 0 0 0 0 0 0 # allocate static space for output
 .text
 main:
     # Load addresses of input matrices (which are in static memory), and set their dimensions
-    
+    la s0 m0
+    la s1 m1
+    la s2 d
 
-
+    # Set matrix attributes
+    add a0, s0, x0
+    addi a1, x0, 3
+    addi a2, x0, 3
+    add a3, s1, x0
+    addi a4, x0, 3
+    addi a5, x0, 3
+    add a6, s2, x0
 
     # Call matrix multiply, m0 * m1
-    
-
-
+    jal matmul
 
     # Print the output (use print_int_array in utils.s)
-
-
-
+    # Print m0 after running relu
+    mv a0 s2
+    li a1 3
+    li a2 3
+    jal print_int_array
 
     # Exit the program
     jal exit
