5d3611be90ede64d4fa62a56104552a048b43d77
taylover2016 <taylover2016@yahoo.com>
2023-01-20 12:45:46 +0800

    read_matrix implemented

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,16 +26,114 @@
 read_matrix:
 
     # Prologue
-	
+    # s0 -> pointer to the memory storing the matrix
+    # s1 -> pointer to the row of the matrix
+    # s2 -> pointer to the col of the matrix
+    # s3 -> pointer to the filename string / file descriptor
+    addi sp, sp, -24
+    sw ra, 20(sp)
+    sw s4, 16(sp)
+    sw s3, 12(sp)
+    sw s0, 8(sp)
+    sw s1, 4(sp)
+    sw s2, 0(sp)
 
+    # Back up the arguments
+    add s1, a1, x0
+    add s2, a2, x0
+    add s3, a0, x0
 
+    # Open the file
+    add a1, s3, x0
+    li a2, 0
+    jal fopen
 
+    # Exit with error code 50 if fopen fails
+    li t0, -1
+    beq a0, t0, fopen_error
+    add s3, a0, x0
 
+    # Read the rows and cols
+    # First the rows
+    add a1, s3, x0
+    add a2, s1, x0
+    li a3, 4
+    jal fread
 
+    # Exit with error code 51 if fread fails
+    li t0, 4
+    bne a0, t0, fread_error
 
+    # Then the cols
+    add a1, s3, x0
+    add a2, s2, x0
+    li a3, 4
+    jal fread
 
+    # Exit with error code 51 if fread fails
+    li t0, 4
+    bne a0, t0, fread_error
+
+    # Read the row and the col
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+
+    # Calculate the bytes needed for allocation
+    mul t0, t1, t2
+    slli t0, t0, 2
+    add s4, t0, x0
+
+    # Allocate memory for the matrix
+    add a0, s4, x0
+    jal malloc
+
+    # Exit with error code 48 if malloc fails
+    li t0, 0
+    beq a0, t0, malloc_error
+    add s0, a0, x0
+
+    # Read in the matrix
+    add a1, s3, x0
+    add a2, s0, x0
+    add a3, s4, x0
+    jal fread
+
+    # Exit with error code 51 if fread fails
+    bne a0, s4, fread_error
+
+    # Close the file
+    add a0, s3, x0
+    jal fclose
+
+    # Exit with error code 52 if fclose fails
+    bne a0, x0, fclose_error
+
+    # Return the pointer to caller
+    add a0, s0, x0
 
     # Epilogue
-
+    lw ra, 20(sp)
+    lw s4, 16(sp)
+    lw s3, 12(sp)
+    lw s0, 8(sp)
+    lw s1, 4(sp)
+    lw s2, 0(sp)
+    addi sp, sp, 24
 
     ret
+
+malloc_error:
+    addi a1, x0, 48
+    j exit2
+
+fopen_error:
+    addi a1, x0, 50
+    j exit2
+
+fread_error:
+    addi a1, x0, 51
+    j exit2
+
+fclose_error:
+    addi a1, x0, 52
+    j exit2
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -7,11 +7,27 @@ file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
 .text
 main:
     # Read matrix into memory
+    # Save the row and the col to memory
+    li t0, 3
+    li t1, 3
+    addi sp, sp, -8
+    sw t0, 4(sp)
+    sw t1, 0(sp)
 
-
+    # Set the arguments and call the function
+    la a0 file_path
+    addi t2, sp, 4
+    add a1, t2, x0
+    add a2, sp, x0
+    jal read_matrix
 
     # Print out elements of matrix
-
-
+    #mv a0 s2
+    li a1 3
+    li a2 3
+    jal print_int_array
 
     # Terminate the program
+    addi sp, sp, 8
+    jal exit
+    
